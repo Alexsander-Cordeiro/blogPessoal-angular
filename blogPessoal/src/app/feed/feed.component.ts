9d8c66f033da63caa1fadb5c6da4a37745edf025
Alexsander-Cordeiro <alexdex12@hotmail.com>
2020-05-06 13:38:49 -0300

    Implementação do endPoint deletar postagens

--- blogPessoal/src/app/feed/feed.component.ts
+++ blogPessoal/src/app/feed/feed.component.ts
@@ -16,15 +16,35 @@ export class FeedComponent implements OnInit {
 
   postagem: Postagem = new Postagem;
 
+  alerta: boolean = false
+
   // Tudo que fazer no CONSTRUTOR é injeção de dependência
   constructor(private postagemService: PostagemService) { }
 
   // Tudo que fizer no ngOnInit é aquilo que vai ser carregado quando abrir minha aplicação.
   ngOnInit() {
     this.findallPostagens()
+
+    let item:string = localStorage.getItem('delOk')
+
+    if(item == "true"){
+      this.alerta = true
+      localStorage.clear()
+
+      // Depois de 3 segundos a pagina vai ser atualizada
+      setTimeout(()=>{
+        location.assign('/feed') 
+      }, 3000)
+     
+    }
+
+
   }
                               // subscribe ele inseri na variavel "resp" o objeto que quero enviar.
   findallPostagens(){
+
+    window.scroll(0,0)
+
     this.postagemService.getAllPostagens().subscribe((resp: Postagem []) => {
       this.listaPostagens = resp
     })
